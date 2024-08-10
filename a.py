0c59c8e76e32b599cdf2400b16b879c0ccc35e9f
flyindulger <100856157+flyindulger@users.noreply.github.com>
2024-08-10 16:21:42 +0800

    ljl667

--- a.py
+++ a.py
@@ -1,19 +1,19 @@
 from PIL import Image
 
 # 打开要翻转的图片
-path = f'a.jpg'
-image = Image.open(path)
-
-# 设置压缩质量（0-100），数值越低，压缩效果越明显
-quality = 30
-
-# 保存压缩后的图片，保留原分辨率
-image.save(path, 'JPEG', quality=quality)
-# path = './img/55.jpg'
+# path = f'a.jpg'
 # image = Image.open(path)
 
-# # 将图像顺时针旋转90度
-# rotated_image = image.rotate(-90, expand=True)
+# # 设置压缩质量（0-100），数值越低，压缩效果越明显
+# quality = 30
 
-# # 保存翻转后的图片
-# rotated_image.save(path)
+# # 保存压缩后的图片，保留原分辨率
+# image.save('b.jpg', 'JPEG', quality=quality)
+path = 'b.jpg'
+image = Image.open(path)
+
+# 将图像顺时针旋转90度
+rotated_image = image.rotate(-90, expand=True)
+
+# 保存翻转后的图片
+rotated_image.save(path)
